828fb2c83e88de34ab69c4d19eae7320e9ac6ddd
mggoro <martingoro@gmail.com>
2022-06-14 21:28:00 -0300

    prueba 2

--- index.php
+++ index.php
@@ -5,6 +5,6 @@
  <body>
  <?php echo '<p>Hola Argentina</p>'; ?>
  <?php echo '<p>Soy Nacho</p>'; ?>
-  <?php echo '<p>Hay una manera distinta</p>'; ?>
+  <?php echo '<p>Hay una manera distinta y modificamos desde nuestra pc segunda modificacion</p>'; ?>
  </body>
 </html>
